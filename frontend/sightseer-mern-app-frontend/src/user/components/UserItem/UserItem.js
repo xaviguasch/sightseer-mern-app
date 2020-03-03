cc464bd054ced04871d508e4e64900f87791a40f
xaviguasch <xaviguasch@gmail.com>
2020-03-03 12:27:28 +0100

    added UsersList page and logic

---
+++ frontend/sightseer-mern-app-frontend/src/user/components/UserItem/UserItem.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './UserItem.css'
+
+const UserItem = () => {
+  return <div></div>
+}
+
+export default UserItem
--- frontend/sightseer-mern-app-frontend/src/user/pages/Users.js
+++ frontend/sightseer-mern-app-frontend/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import UsersList from '../components/UsersList/UsersList'
 
 const Users = () => {
-  return <h2>Users Works!</h2>
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Xavi Guasch',
+      image:
+        'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2389&q=80',
+      places: 3
+    }
+  ]
+
+  return <UsersList items={USERS} />
 }
 
 export default Users
