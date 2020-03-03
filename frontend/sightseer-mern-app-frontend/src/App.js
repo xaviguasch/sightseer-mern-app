1a6c882545863a481d73fd8c80e0251db1be476a
xaviguasch <xaviguasch@gmail.com>
2020-03-03 22:37:51 +0100

    rendered user places

--- frontend/sightseer-mern-app-frontend/src/App.js
+++ frontend/sightseer-mern-app-frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
-import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation'
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
+import UserPlaces from './places/pages/UserPlaces'
+import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation'
 
 const App = () => {
   return (
@@ -13,6 +14,9 @@ const App = () => {
         <Switch>
           <Route path='/' exact>
             <Users />
+          </Route>
+          <Route path='/:userId/places' exact>
+            <UserPlaces />
           </Route>
           <Route path='/places/new' exact>
             <NewPlace />
