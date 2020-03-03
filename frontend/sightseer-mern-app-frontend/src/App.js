01b48e02105b8b9ef87f5fc375032dee0e069de2
xaviguasch <xaviguasch@gmail.com>
2020-03-03 11:34:44 +0100

    toyed with react router

--- frontend/sightseer-mern-app-frontend/src/App.js
+++ frontend/sightseer-mern-app-frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+
+import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace'
 
 const App = () => {
   return (
     <Router>
-      <h1>Sightseer App</h1>
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/places/new' exact>
+          <NewPlace />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
     </Router>
   )
 }
