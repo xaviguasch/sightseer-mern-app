2d3a0fc83348b30acc8111d8c71db49de3b9b8f0
xaviguasch <xaviguasch@gmail.com>
2020-03-04 12:29:29 +0100

    api test

---
+++ frontend/sightseer-mern-app-frontend/src/shared/components/UIElements/Map/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Map.css'
+
+const Map = props => {
+  return <div className={`map ${props.className}`} style={props.style}></div>
+}
+
+export default Map
+
+// AIzaSyCaGhhwYqeGX0o4tX13qs6YefEiaOZ_xzg
