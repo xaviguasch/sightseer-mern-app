18002f0fa6317bfe75c19affd040fc948a14f6c4
xaviguasch <xaviguasch@gmail.com>
2020-03-04 13:03:56 +0100

    added map with google maps

--- frontend/sightseer-mern-app-frontend/src/places/components/PlaceItem/PlaceItem.js
+++ frontend/sightseer-mern-app-frontend/src/places/components/PlaceItem/PlaceItem.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import Card from '../../../shared/components/UIElements/Card/Card'
 import Button from '../../../shared/components/FormElements/Button/Button'
 import Modal from '../../../shared/components/UIElements/Modal/Modal'
+import Map from '../../../shared/components/UIElements/Map/Map'
 
 import './PlaceItem.css'
 
@@ -24,7 +25,7 @@ const PlaceItem = props => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className='map-container'>
-          <h2>THE MAP!</h2>
+          <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
       <li className='place-item'>
