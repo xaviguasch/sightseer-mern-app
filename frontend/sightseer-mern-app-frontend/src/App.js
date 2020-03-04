237f0883367226e3d34aaffad69c27e7c6128539
xaviguasch <xaviguasch@gmail.com>
2020-03-04 17:03:42 +0100

    added custom form input component

--- frontend/sightseer-mern-app-frontend/src/App.js
+++ frontend/sightseer-mern-app-frontend/src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
 import Users from './user/pages/Users'
-import NewPlace from './places/pages/NewPlace'
-import UserPlaces from './places/pages/UserPlaces'
+import NewPlace from './places/pages/NewPlace/NewPlace'
+import UserPlaces from './places/pages/UserPlaces/UserPlaces'
 import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation'
 
 const App = () => {
--- frontend/sightseer-mern-app-frontend/src/places/pages/UserPlaces/UserPlaces.js
+++ frontend/sightseer-mern-app-frontend/src/places/pages/UserPlaces/UserPlaces.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-import PlaceList from '../components/PlaceList/PlaceList'
+import PlaceList from '../../components/PlaceList/PlaceList'
 
 const DUMMY_PLACES = [
   {
