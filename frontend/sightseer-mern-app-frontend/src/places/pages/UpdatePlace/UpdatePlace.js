9f1479f5b3e02ddf1ef835d5c324015fdcebb5fd
xaviguasch <xaviguasch@gmail.com>
2020-03-05 10:33:17 +0100

    adjusted the input component

--- frontend/sightseer-mern-app-frontend/src/places/pages/UpdatePlace/UpdatePlace.js
+++ frontend/sightseer-mern-app-frontend/src/places/pages/UpdatePlace/UpdatePlace.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 import Input from '../../../shared/components/FormElements/Input/Input'
 import Button from '../../../shared/components/FormElements/Button/Button'
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../../shared/util/validators'
+import '../PlaceForm.css'
 
 const DUMMY_PLACES = [
   {
@@ -48,7 +49,7 @@ const UpdatePlace = () => {
   }
 
   return (
-    <form>
+    <form className='place-form'>
       <Input
         id='title'
         element='input'
