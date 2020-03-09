b26c2c18d1071216a642d3289f125f2b8dc06cd5
xaviguasch <xaviguasch@gmail.com>
2020-03-09 21:57:22 +0100

    added place specific routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,10 +2,26 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  console.log('GET request in places')
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.768686,
+      lng: -73.9876733
+    },
+    address: '20 W 3th St, New York, NY 10001',
+    creator: 'u1'
+  }
+]
 
-  res.json({ message: 'It works!' })
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid
+
+  const place = DUMMY_PLACES.find(p => p.id === placeId)
+
+  res.json({ place })
 })
 
 module.exports = router
