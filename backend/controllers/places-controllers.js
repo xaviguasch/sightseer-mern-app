c6f7230c56fe9c8e1b4a6cff3bc57d451191f856
xaviguasch <xaviguasch@gmail.com>
2020-03-15 23:26:54 +0100

    validated the API input in the request bodies

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v4')
+const { validationResult } = require('express-validator')
 
 const HttpError = require('../models/http-error')
 
@@ -41,6 +42,13 @@ const getPlacesByUserId = (req, res, next) => {
 }
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    console.log(errors)
+    throw new HttpError('Invalids inputs passed, please check your data.', 422)
+  }
+
   const { title, description, coordinates, address, creator } = req.body
 
   const createdPlace = {
