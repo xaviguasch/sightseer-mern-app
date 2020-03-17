d7538e96727ada9e3014fa5056658c573f8aee61
xaviguasch <xaviguasch@gmail.com>
2020-03-17 13:41:11 +0100

    converted an address to coordinates via Geocoding API

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -2,6 +2,7 @@ const uuid = require('uuid/v4')
 const { validationResult } = require('express-validator')
 
 const HttpError = require('../models/http-error')
+const getCoordsForAddress = require('../util/location')
 
 let DUMMY_PLACES = [
   {
@@ -41,15 +42,21 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places })
 }
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
-    console.log(errors)
-    throw new HttpError('Invalids inputs passed, please check your data.', 422)
+    next(new HttpError('Invalids inputs passed, please check your data.', 422))
   }
 
-  const { title, description, coordinates, address, creator } = req.body
+  const { title, description, address, creator } = req.body
+
+  let coordinates
+  try {
+    coordinates = await getCoordsForAddress(address)
+  } catch (error) {
+    return next(error)
+  }
 
   const createdPlace = {
     id: uuid(),
