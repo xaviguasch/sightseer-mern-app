f1c8a5986850f9238fbf326476b2739ea248663d
xaviguasch <xaviguasch@gmail.com>
2020-04-01 22:21:13 +0200

    gotten places from user ID

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -31,27 +31,34 @@ const getPlaceById = async (req, res, next) => {
   }
 
   if (!place) {
-    throw new HttpError('Could not find a place for the provided id.', 404)
+    const error = new HttpError('Could not find a place for the provided id.', 404)
+    return next(error)
   }
 
-  res.json({ place }) // => { place } => { place: place }
+  res.json({ place: place.toObject({ getters: true }) }) // => { place } => { place: place }
 }
 
 // function getPlaceById() { ... }
 // const getPlaceById = function() { ... }
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid
 
-  const places = DUMMY_PLACES.filter(p => {
-    return p.creator === userId
-  })
+  let places
+
+  try {
+    places = await Place.find({ creator: userId })
+  } catch (err) {
+    const error = new HttpError('Fetching places failed, please try again later', 500)
+
+    return next(error)
+  }
 
   if (!places || places.length === 0) {
     return next(new HttpError('Could not find places for the provided user id.', 404))
   }
 
-  res.json({ places })
+  res.json({ places: places.map(place => place.toObject({ getters: true })) })
 }
 
 const createPlace = async (req, res, next) => {
