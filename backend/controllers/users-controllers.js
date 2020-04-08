4de23b0add90017ec403110d2580b195a6efdf47
xaviguasch <xaviguasch@gmail.com>
2020-04-08 22:22:06 +0200

    added the User login

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -59,13 +59,21 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) })
 }
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body
 
-  const identifiedUser = DUMMY_USERS.find(user => user.email === email)
+  let existingUser
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('Could not identify user, credentials seem to be wrong.', 401)
+  try {
+    existingUser = await User.findOne({ email: email })
+  } catch (err) {
+    const error = new HttpError('Logging in failed, plese try again later.', 500)
+    return next(error)
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError('Invalid credentials, could not log you in.', 401)
+    return next(error)
   }
 
   res.json({ message: 'Logged in!' })
